0c84036c63b9cef85cc237ba6b098cde1d50202a
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-05-26 22:05:06 +0530

    mified the js files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,126 +1,92 @@
-/*
-	Miniport by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
-(function ($) {
-  $(function () {
-    console.log("form fields touched");
-    $("#emailError").hide();
-    $("#subjectError").hide();
-    $("#form-submit-message").hide();
-    var error_email = false;
-
-    $("#name").focusin(function () {
-      $("#form-submit-error").hide();
-      $("#form-submit-message").hide();
+!(function (e) {
+  e(function () {
+    console.log("form fields touched"),
+      e("#emailError").hide(),
+      e("#subjectError").hide(),
+      e("#form-submit-message").hide();
+    e("#name").focusin(function () {
+      e("#form-submit-error").hide(), e("#form-submit-message").hide();
+    }),
+      e("#email").focusin(function () {
+        e("#form-submit-error").hide(), e("#form-submit-message").hide();
+      }),
+      e("#subject").focusin(function () {
+        e("#form-submit-error").hide(), e("#form-submit-message").hide();
+      }),
+      e("#message").focusin(function () {
+        e("#form-submit-error").hide(), e("#form-submit-message").hide();
+      }),
+      e("#email").focusout(function () {
+        new RegExp(/^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i).test(
+          e("#email").val()
+        )
+          ? e("#emailError").hide()
+          : (console.log("invalid email"),
+            e("#emailError").html("Please enter a valid email id."),
+            e("#emailError").show(),
+            !0);
+      }),
+      e("#subject").keypress(function () {
+        if (this.value.length > 50) return e("#subjectError").hide(), !1;
+        e("#subject").focusout(function () {
+          e("#subjectError").hide();
+        }),
+          e("#subjectError").html(
+            "Remaining characters : " + (100 - this.value.length)
+          ),
+          e("#subjectError").show();
+      });
+  }),
+    e("#contact-form").submit(function (r) {
+      r.preventDefault();
+      var i = e("#email").val(),
+        o = e("#subject").val(),
+        s = e("#message").val(),
+        m = e("#name").val();
+      e("#name").val(""),
+        e("#email").val(""),
+        e("#subject").val(""),
+        e("#message").val(""),
+        i && o && s && m
+          ? (e("#form-submit-message").html(
+              "I have recevied your message! Thanks for writing."
+            ),
+            e("#form-submit-error").hide(),
+            e("#form-submit-message").show())
+          : (e("#form-submit-error").html(
+              "Please fill in the details before submitting."
+            ),
+            e("#form-submit-error").show());
+    }),
+    e("#clear-form").click(function () {
+      console.log("clear form"),
+        e("#name").val(""),
+        e("#email").val(""),
+        e("#subject").val(""),
+        e("#message").val(""),
+        e("#form-submit-error").hide(),
+        e("#form-submit-message").hide(),
+        e("#emailError").hide(),
+        e("#subjectError").hide();
     });
-    $("#email").focusin(function () {
-      $("#form-submit-error").hide();
-      $("#form-submit-message").hide();
-    });
-    $("#subject").focusin(function () {
-      $("#form-submit-error").hide();
-      $("#form-submit-message").hide();
-    });
-    $("#message").focusin(function () {
-      $("#form-submit-error").hide();
-      $("#form-submit-message").hide();
-    });
-
-    //email error
-    $("#email").focusout(function () {
-      var pattern = new RegExp(/^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i);
-      if (pattern.test($("#email").val())) {
-        $("#emailError").hide();
-      } else {
-        console.log("invalid email");
-        $("#emailError").html("Please enter a valid email id.");
-        $("#emailError").show();
-        error_email = true;
-      }
-    });
-
-    // subject error
-    $("#subject").keypress(function () {
-      if (this.value.length > 50) {
-        $("#subjectError").hide();
-        return false;
-      }
-      if (
-        $("#subject").focusout(function () {
-          $("#subjectError").hide();
-        })
-      );
-      $("#subjectError").html(
-        "Remaining characters : " + (100 - this.value.length)
-      );
-      $("#subjectError").show();
-    });
-  });
-
-  $("#contact-form").submit(function (event) {
-    event.preventDefault();
-    var emailInput = $("#email").val();
-    var subjectInput = $("#subject").val();
-    var textMsgInput = $("#message").val();
-
-    var nameInput = $("#name").val();
-
-    $("#name").val("");
-    $("#email").val("");
-    $("#subject").val("");
-    $("#message").val("");
-    if (emailInput && subjectInput && textMsgInput && nameInput) {
-      $("#form-submit-message").html(
-        "I have recevied your message! Thanks for writing."
-      );
-      $("#form-submit-error").hide();
-      $("#form-submit-message").show();
-    } else {
-      $("#form-submit-error").html(
-        "Please fill in the details before submitting."
-      );
-      $("#form-submit-error").show();
-    }
-  });
-  $("#clear-form").click(function () {
-    console.log("clear form");
-    $("#name").val("");
-    $("#email").val("");
-    $("#subject").val("");
-    $("#message").val("");
-    $("#form-submit-error").hide();
-    $("#form-submit-message").hide();
-    $("#emailError").hide();
-    $("#subjectError").hide();
-  });
-
-  var $window = $(window),
-    $body = $("body"),
-    $nav = $("#nav");
-
-  // Breakpoints.
+  var r = e(window),
+    i = e("body"),
+    o = e("#nav");
   breakpoints({
     xlarge: ["1281px", "1680px"],
     large: ["981px", "1280px"],
     medium: ["737px", "980px"],
     small: [null, "736px"],
-  });
-
-  // Play initial animations on page load.
-  $window.on("load", function () {
-    window.setTimeout(function () {
-      $body.removeClass("is-preload");
-    }, 100);
-  });
-
-  // Scrolly.
-  $("#nav a, .scrolly").scrolly({
-    speed: 1000,
-    offset: function () {
-      return $nav.height();
-    },
-  });
+  }),
+    r.on("load", function () {
+      window.setTimeout(function () {
+        i.removeClass("is-preload");
+      }, 100);
+    }),
+    e("#nav a, .scrolly").scrolly({
+      speed: 1e3,
+      offset: function () {
+        return o.height();
+      },
+    });
 })(jQuery);
