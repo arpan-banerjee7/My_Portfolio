812ff9736e52c1914d2144390844e1581759f04c
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-06-19 19:15:16 +0530

    added blog section

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,6 +141,43 @@
     window.setTimeout(function () {
       $body.removeClass("is-preload");
     }, 100);
+
+    $(".post-wrapper").slick({
+      slidesToShow: 2,
+      //add scrooll
+      autoplay: true,
+      autoplaySpeed: 2000,
+      nextArrow: $(".next"),
+      prevArrow: $(".prev"),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ],
+    });
   });
 
   // Scrolly.
