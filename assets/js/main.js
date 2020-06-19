36a3eadaebae886fb04b439ae7f97a671b45ac71
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-06-19 19:58:32 +0530

    added style for desktop view

--- assets/js/main.js
+++ assets/js/main.js
@@ -153,8 +153,8 @@
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 2,
+            slidesToScroll: 2,
             infinite: true,
             dots: true,
           },
