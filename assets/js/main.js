3c4f938d25754a6c99f3084f6ed328f6a9980abd
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-06-19 19:56:18 +0530

    added style for desktop view in mob

--- assets/js/main.js
+++ assets/js/main.js
@@ -173,6 +173,15 @@
             slidesToScroll: 1,
           },
         },
+        {
+          breakpoint: 2000,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true,
+          },
+        },
         // You can unslick at a given breakpoint now by adding:
         // settings: "unslick"
         // instead of a settings object
